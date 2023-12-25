de3a1963374d4ff7c17873ae740d5a33e74b3afd
Hui Lin <hugh80082@gmail.com>
2023-12-25 15:54:21 -0800

    Add intro and update bio

--- src/app/config/texts.js
+++ src/app/config/texts.js
@@ -1,6 +1,8 @@
 export const Title = "Hello, I'm Hui";
-export const Intro = 'Intro goes here';
-export const Bio = `I am a full stack web developer with a passion for creating interactive and responsive web applications. I have experience working with JavaScript, React, Redux, Node.js, Express, PostgreSQL, Sequelize, HTML, CSS, and Git. I am a quick learner and I am always looking to expand my knowledge and skill set. I am a team player and I am excited to work with others to create amazing applications.`;
+export const Intro =
+  "I am a recent CS graduate looking for opportunities in the software industry. I like spending time building web applications. When I'm not coding I like listening to music and podcasts. I'm also a fan of anime and soccer.";
+export const Bio = `I am a front end and full stack web developer passionate for creating interactive and responsive web applications. I have experience working with JavaScript, React.js, Redux, Three.js, Node.js, Express, MongoDB, Sequelize, MySQL, HTML, CSS, and Git. I'm proficient in programming languages such as JavaScript, Python, and C++. I am a quick learner and always looking to expand my skillsets. I am also a team player and am looking forward to work with others to create fluid and beautiful applications.`;
+
 export const ProjectsData = [
   {
     title: "I'm Fine With Whatever",
